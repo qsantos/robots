b24b066cc04145dffca59f1a2958b36166da8fad
Quentin SANTOS <quentin.santos@worldonline.fr>
2012-11-07 21:03:19 +0100

    Added RobotOrder to protocol to track robots' orders

--- game.h
+++ game.h
@@ -86,6 +86,13 @@ typedef struct __attribute__((packed))
 
 typedef struct __attribute__((packed))
 {
+	float advance;
+	float turn;
+	float turnGun;
+} RobotOrder;
+
+typedef struct __attribute__((packed))
+{
 	u32   from;
 	float x;
 	float y;
